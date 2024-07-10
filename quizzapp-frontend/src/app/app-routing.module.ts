5e8a1e97d97650efd2e03b405ceeca6e1955a23f
d4rksan00 <jonas.edler@compusafe.de>
2024-07-10 16:51:49 +0200

    added quizround to homepage

--- quizzapp-frontend/src/app/app-routing.module.ts
+++ quizzapp-frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { GameOverviewComponent } from './game-overview/game-overview.component';
 import { PageDoesNotExistComponent } from './page-does-not-exist/page-does-not-exist.component';
+import { QuizRoundComponent } from './quiz-round/quiz-round.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'homepage', component: HomepageComponent, 
     children: [
-      {path: 'overview', component: GameOverviewComponent}
+      {path: 'overview', component: GameOverviewComponent},
+      {path: 'quizround', component: QuizRoundComponent}
    ]
   },
   {path: '**', component: PageDoesNotExistComponent}
