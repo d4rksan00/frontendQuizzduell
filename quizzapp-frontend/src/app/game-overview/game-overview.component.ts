5847268955c854e4b13da3e2357e13f7780d6788
d4rksan00 <jonas.edler@compusafe.de>
2024-07-11 13:48:23 +0200

    changed a view things

--- quizzapp-frontend/src/app/game-overview/game-overview.component.ts
+++ quizzapp-frontend/src/app/game-overview/game-overview.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-game-overview',
   templateUrl: './game-overview.component.html',
@@ -7,4 +14,20 @@ import { Component } from '@angular/core';
 })
 export class GameOverviewComponent {
 
+  tiles: Tile[] = [
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Four', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Four', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Four', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Four', cols: 1, rows: 1, color: 'lightblue'},
+  ];
+
+  winClick() {
+    const element: HTMLElement | null = document.getElementById('button-1');
+    if (element) {
+      element.style.backgroundColor = 'green';
+    }
+  }
 }
