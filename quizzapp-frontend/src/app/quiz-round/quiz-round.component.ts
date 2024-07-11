0f3e05be649fafc1d49ce29183fec26a2c8f299b
Viktor <viktor.koevesdi@compusafe.de>
2024-07-11 08:39:15 +0200

    Basic 30 sec timer added

--- quizzapp-frontend/src/app/quiz-round/quiz-round.component.ts
+++ quizzapp-frontend/src/app/quiz-round/quiz-round.component.ts
@@ -1,9 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {interval} from "rxjs";
 
 @Component({
   selector: 'app-quiz-round',
   templateUrl: './quiz-round.component.html',
   styleUrl: './quiz-round.component.css',
 })
-export class QuizRoundComponent implements MatProgressBarModule{}
+export class QuizRoundComponent implements MatProgressBarModule, OnInit{
+
+  progressbarValue = 100;
+  curSec: number = 0;
+
+  startTimer(seconds: number) {
+    const time = seconds;
+    const timer$ = interval(1000);
+
+    const sub = timer$.subscribe((sec) => {
+      this.progressbarValue = 100 - sec * 100 / seconds;
+      this.curSec = sec;
+
+      if (this.curSec === seconds) {
+        sub.unsubscribe();
+      }
+    });
+  }
+
+  ngOnInit() {this.startTimer(30)
+  }
+}
