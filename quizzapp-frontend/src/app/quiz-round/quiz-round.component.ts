c4a3fea567422c19f568fd0dfdd84b6cdbae4d99
Viktor <viktor.koevesdi@compusafe.de>
2024-07-22 11:53:09 +0200

    fixed dynamic q/a's

--- quizzapp-frontend/src/app/quiz-round/quiz-round.component.ts
+++ quizzapp-frontend/src/app/quiz-round/quiz-round.component.ts
@@ -10,54 +10,44 @@ import {QuestionService} from "../service/question.service";
   styleUrl: './quiz-round.component.css',
   providers: [QuestionService]
 })
-export class QuizRoundComponent implements MatProgressBarModule, OnInit{
+
+export class QuizRoundComponent implements MatProgressBarModule, OnInit {
 
   // Question variables
   questions: Question[] = [];
-  questionService: QuestionService
-  shuffledAnswersOne: string[] = [];
-  shuffledAnswersTwo: string[] = [];
-  shuffledAnswersThree: string[] = [];
-  isFirst : boolean = true;
-  isSecond : boolean = false;
-  isThird : boolean = false;
+  currentQuestion: Question | undefined;
+  questionService: QuestionService;
+  shuffledAnswers: string[] = [];
 
   // Timer variables
   progressbarValue = 100;
   curSec: number = 0;
 
   // "clicked" variables
-  answered : boolean = false;
-  isVisible : boolean = false;
+  answered: boolean = false;
+  isVisible: boolean = false;
 
   // constructor
   constructor(questionService: QuestionService) {
     this.questionService = questionService;
   }
 
-  htmlDecode(input: string):string {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(input, 'text/html');
-    return doc.documentElement.textContent!;
-  }
-
   // Get questions from requested category
   public getQuestions() {
     return this.questionService.getQuestions().subscribe((response) => {
       this.questions = response.results;
+      this.currentQuestion = this.questions[0]
 
-      this.shuffledAnswersOne = this.shuffledAnswersOne.concat(this.questions[0]?.incorrect_answers)
-      this.shuffledAnswersOne.push(this.questions[0]?.correct_answer)
-      this.shuffledAnswersOne.sort(()=>Math.random()-0.5);
-
-      this.shuffledAnswersTwo = this.shuffledAnswersTwo.concat(this.questions[1]?.incorrect_answers)
-      this.shuffledAnswersTwo.push(this.questions[1]?.correct_answer)
-      this.shuffledAnswersTwo.sort(()=>Math.random()-0.5);
-
-      this.shuffledAnswersThree = this.shuffledAnswersThree.concat(this.questions[2]?.incorrect_answers)
-      this.shuffledAnswersThree.push(this.questions[2]?.correct_answer)
-      this.shuffledAnswersThree.sort(()=>Math.random()-0.5);
+      this.shuffleAnswers(this.currentQuestion)
     })
+  }
+
+  // Shuffle possible answers
+  shuffleAnswers(question: Question | undefined) {
+    this.shuffledAnswers.length = 0
+    this.shuffledAnswers = this.shuffledAnswers.concat(question?.incorrect_answers!)
+    this.shuffledAnswers.push(question?.correct_answer!)
+    this.shuffledAnswers.sort(() => Math.random() - 0.5);
   }
 
   // Timer bar
@@ -73,28 +63,44 @@ export class QuizRoundComponent implements MatProgressBarModule, OnInit{
 
       if (this.curSec === time || this.answered) {
         sub.unsubscribe();
+      } if (this.progressbarValue === 0 && !this.answered) {
+        alert("Time has run out!")
+        sub.unsubscribe();
+        this.isVisible = true;
       }
     });
-  }
-
-  //set question flag
-  setQuestionFlag (questionNr : boolean) {
-    return !questionNr
   }
 
   //"clicked" event
   clicked(answer: string) {
     this.answered = true;
     this.isVisible = true;
-    if (answer === this.questions[0]?.correct_answer) {
+
+    if (answer === this.currentQuestion?.correct_answer) {
       alert("Your answer is correct!");
     } else {
       alert("Unfortunately your answer is false!");
     }
   }
 
+  //jump to next question
+  nextQuestion(index: number) {
+    this.currentQuestion = this.questions[index + 1]
+    this.isVisible = false;
 
-  ngOnInit() {this.startTimer(30)
+    this.startTimer(30)
+    this.shuffleAnswers(this.currentQuestion)
+  }
+
+  // HTML Decoder DOMParser
+  htmlDecode(input: string): string {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(input, 'text/html');
+    return doc.documentElement.textContent!;
+  }
+
+  ngOnInit() {
+    this.startTimer(30)
     this.getQuestions()
-    }
+  }
 }
